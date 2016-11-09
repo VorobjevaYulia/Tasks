5f0930fa62a1ff49341a8bfe665c6f32de60810f
vorobjeva.yulia <vorobjeva.ulia@gmail.com>
2016-11-09 23:55:49 +0300

    attempt

---
+++ task1.c
@@ -0,0 +1,17 @@
+#include "stdafx.h" 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <iostream> 
+using namespace std;
+
+
+int main() {
+	int x, y; 
+	cout<< "X = ";
+	cin>> x;
+	cout<< "Y = "; 
+	cin>> y;
+
+	cout<< "x+y = "<<(x + y)<<std::endl;
+	return 0;
+}
